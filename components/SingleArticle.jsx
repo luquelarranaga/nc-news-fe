7d1d572b69df2439943601f8587e5e5a110ac448
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-02-23 22:12:19 +0000

    created two buttons for the singleArticle component. Added image for the upvote button, and added functionality so that when a user clicks on it it increase votes by 1

--- components/SingleArticle.jsx
+++ components/SingleArticle.jsx
@@ -6,6 +6,7 @@ function SingleArticle() {
   const articleIdUrl = `https://back-end-nc-news-yvh9.onrender.com/api/articles/${article_id}`;
 
   const [singleArticle, setArticle] = useState({});
+  const [votes, setVote] = useState(0);
   useEffect(() => {
     async function getArticle() {
       const response = await fetch(articleIdUrl);
@@ -13,9 +14,14 @@ function SingleArticle() {
       const { article } = await response.json();
       console.log("ind article>>", article);
       setArticle(article);
+      setVote(article.votes);
     }
     getArticle();
   }, []);
+
+  function upVote() {
+    setVote(votes + 1);
+  }
 
   return (
     <section className="single-article">
@@ -33,13 +39,23 @@ function SingleArticle() {
           <h3 className="article-title">{singleArticle.title}</h3>
           <h5 className="article-topic">{singleArticle.topic}</h5>
           <h6 className="article-comments-votes">
-            comments {singleArticle.total_comments}, {singleArticle.votes}
+            comments {singleArticle.total_comments}, {votes}
           </h6>
         </div>
       </section>
       <div className="article-body">
         <p>{singleArticle.body}</p>
       </div>
+      <section className="comment-buttons">
+        <button className="upvote-button" type="button" onClick={upVote}>
+          <img
+            className="upvote-img"
+            src="../src/assets/upvote2.png"
+            alt="upvote"
+          />
+        </button>
+        <button type="button">new comment</button>
+      </section>
     </section>
   );
 }
