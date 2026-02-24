6ee7f9b469d1ad6c823b0bfc46baa92f1be09740
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-02-24 10:32:00 +0000

    created a function within Comments component that fetches the comments for a given article id from the database

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useParams } from "react-router";
+
 function Comments() {
+  const { article_id } = useParams();
+  const commentsUrl = `https://back-end-nc-news-yvh9.onrender.com/api/articles/${article_id}/comments`;
+
+  useEffect(() => {
+    async function getComments() {
+      const response = await fetch(commentsUrl);
+      const comments = await response.json();
+      console.log("all comments>>", comments);
+    }
+    getComments();
+  });
   return <>comments!</>;
 }
 
