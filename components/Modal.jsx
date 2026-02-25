39c5235bcab06c128ecad2a61aaf60c596786796
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-02-25 10:54:01 +0000

    added default user in Modal component. Created inputValue and invalidInput states, which were then used to handle the submit of the comment form. The handlesubmit function is yet to be completed

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 function Modal() {
+  const [inputValue, setInputValue] = useState("");
+  const [invalidInput, setInvalidInput] = useState(true);
+
+  const user = {
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
+  };
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (inputValue === "") {
+      setInvalidInput(true);
+    } else {
+      setInvalidInput(false);
+    }
+
+    const newComment = {
+      username: user.name,
+      body: inputValue,
+    };
+
+    console.log("form json>>>", inputValue);
+    console.log("new comment>>>", newComment);
+
+    setInputValue("");
   }
+
+  function handleChange(e) {
+    setInputValue(e.target.value);
+  }
+
   return (
     <section className="modal">
       <form onSubmit={handleSubmit}>
@@ -11,6 +43,8 @@ function Modal() {
           id="comment"
           rows={5}
           placeholder="enter your comment here"
+          value={inputValue}
+          onChange={handleChange}
         />{" "}
         <button type="submit" id="post-comment-button">
           post comment
