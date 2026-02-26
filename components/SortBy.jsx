200ac1f82125327d1414635a6dff1b176656b856
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-02-26 18:56:15 +0000

    added a Select component within my SortBy component which allows users to select how they want to sort the given articles

--- components/SortBy.jsx
+++ components/SortBy.jsx
@@ -1,3 +1,25 @@
-function SortBy() {}
+import React from "react";
+import Select from "react-select";
+import { useState } from "react";
+
+function SortBy() {
+  const [selectedOption, setSelectedOption] = useState({
+    value: "Most Popular",
+    label: "Most Popular",
+  });
+  const options = [
+    { value: "Most Popular", label: "Most Popular" },
+    { value: "Most Recent", label: "Most Recent" },
+    { value: "A-Z", label: "A-Z" },
+  ];
+
+  function handleChange(option) {
+    setSelectedOption(option);
+  }
+
+  return (
+    <Select value={selectedOption} options={options} onChange={handleChange} />
+  );
+}
 
 export default SortBy;
