a570ebec4683c0be856cdce9e701bc50428257d5
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-02-26 15:19:11 +0000

    updated Topic component so that it returns a list of all availible topics

--- components/Topics.jsx
+++ components/Topics.jsx
@@ -1,3 +1,28 @@
-function Topics() {}
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+function Topics() {
+  const [allTopics, setTopics] = useState([]);
+
+  useEffect(() => {
+    async function getTopics() {
+      const { data } = await axios.get(
+        `https://back-end-nc-news-yvh9.onrender.com/api/topics`,
+      );
+      const { topics } = data;
+      setTopics(topics);
+    }
+    getTopics();
+  }, []);
+
+  return (
+    <ul>
+      {allTopics.map((topic) => {
+        return <li>{topic.slug} </li>;
+      })}
+    </ul>
+  );
+}
 
 export default Topics;
