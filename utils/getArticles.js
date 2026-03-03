3eb7464d14eddf47d8163d09248a32e8e38f402a
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-03-03 20:13:15 +0000

    added arguments and return statement

--- utils/getArticles.js
+++ utils/getArticles.js
@@ -1,8 +1,12 @@
-async function getArticles() {
+import axios from "axios";
+
+async function getArticles(sortBy, order) {
   const { data } = await axios(
     `https://back-end-nc-news-yvh9.onrender.com/api/articles?${sortBy}&${order}`,
   );
   const { articles } = data;
+  console.log("articles in getArticles utils function", articles);
+  return articles;
 }
 
 export default getArticles;
