3467f9f572d90e5be27192532bd5186a247de790
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-03-03 20:14:46 +0000

    deleted file, modified gitignore

--- utils/getArticle.js
+++
@@ -1,8 +0,0 @@
-async function getArticle() {
-  const { data } = await axios(
-    `https://back-end-nc-news-yvh9.onrender.com/api/articles/${article_id}`,
-  );
-  const { articles } = data;
-}
-
-export default getArticle;
