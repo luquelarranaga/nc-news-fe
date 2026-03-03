37367f91166545b75eed19aa34917fda00ce28c4
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-03-03 13:06:48 +0000

    created hooks folder and created first hook inside useAxios file

---
+++ components/hooks/useAxios.jsx
@@ -0,0 +1,21 @@
+function useAxios(dataFunction, { deps = [], params = [] }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function setup() {
+      setIsLoading(true);
+      try {
+        const { data } = await dataFunction(...params);
+        setData(data);
+      } catch (err) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setup();
+  }, [...deps]);
+  return { isLoading, error, data };
+}
