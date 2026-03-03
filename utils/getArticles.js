7d533478301c1281fdd13462061bd83f972bfa2d
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-03-03 13:18:50 +0000

    updated url in function, and exported

--- utils/getArticles.js
+++ utils/getArticles.js
@@ -1,4 +1,8 @@
 async function getArticles() {
-  const { data } = await axios(articlesUrl);
+  const { data } = await axios(
+    `https://back-end-nc-news-yvh9.onrender.com/api/articles?${sortBy}&${order}`,
+  );
   const { articles } = data;
 }
+
+export default getArticles;
